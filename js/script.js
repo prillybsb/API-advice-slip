74c3e473141360e9a71a985909309627cca65ef2
Priscila Lessa <prillybsb@gmail.com>
2023-09-06 21:52:22 -0300

    atualizando

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 async function getAdvice() {
-  const resposta = awaitfetch("https://api.adviceslip.com/advice");
+  try {
+    const resposta = await fetch("https://api.adviceslip.com/advice");
 
-  //console.log(resposta)
+    if (!resposta.ok) {
+      throw new Error("Não foi possível obter o conselho.");
+    }
 
-  const data = await resposta.json();
-  //console.log(data)
+    const data = await resposta.json();
+
+    // Selecione o elemento onde você deseja exibir o conselho (com id "advice")
+    const adviceElement = document.querySelector("#advice");
+
+    // Atualize o conteúdo do elemento com o conselho obtido da API
+    adviceElement.innerHTML = data.slip.advice;
+  } catch (error) {
+    console.error("Ocorreu um erro:", error);
+  }
 }
 
+// Botão funcionando
+
+let botaoAdvice = document.querySelector("#get-advice");
+
+botaoAdvice.addEventListener("click", function () {
+  getAdvice();
+});
+
+// Chame a função getAdvice() inicialmente para obter um conselho quando a página carregar
 getAdvice();
